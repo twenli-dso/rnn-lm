f2e57abae870f9916997bc438f2501ad6ca3565a
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-07-20 13:22:16 +0800

    changed output path for results

--- episode_mining_fixed_window.py
+++ episode_mining_fixed_window.py
@@ -234,7 +234,7 @@ while testdate <= '20160502':
     flagged_red_hosts = []
     flagged_non_red_hosts = []
 
-    outfile = "./results/flagged_lines.txt"
+    outfile = "./results/fixed_window_results.txt"
     printline = "\n-- STATISTICS FOR " + str(testdate) + " --\n"
     with open (outfile, 'a') as writefile:
         writefile.write(printline + "\n")
