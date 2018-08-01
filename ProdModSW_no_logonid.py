09b693718ba01fbf9006e2da515afc31dc7ac1e8
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-08-01 14:49:17 +0800

    defined red_flagged_dict

--- ProdModSW_no_logonid.py
+++ ProdModSW_no_logonid.py
@@ -224,6 +224,7 @@ while testdate <= '20160502':
     false_positive_hosts = 0
     flagged_red_hosts = []
     flagged_non_red_hosts = []
+    red_flagged_dict = {}
     non_red_flagged_dict = {}
 
     print ('Total number of logs to process:',len(x_test))
