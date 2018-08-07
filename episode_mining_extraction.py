227198a6c008c76e9cdf534fc5c068b245d8d6ee
wenlitay <wenli.tay.2015@sis.smu.edu.sg>
2018-08-07 10:08:25 +0800

    append eventtype to back of eventid for feature extraction

--- episode_mining_extraction.py
+++ episode_mining_extraction.py
@@ -31,19 +31,26 @@ for url in file_list:
                         break
                     d = json.loads(data)
                     eventtype = d['Log']['Event']['Name']
+                    eventid = d['Log']['Event']['EventID']
                     if eventtype == 'Security':
-                        eventid = d['Log']['Event']['EventID']
-                        hostname = d['Log']['HostName']
-                        timestamp = d['Log']['Event']['Timestamp-Readable-UTC']
-                        logtime = timestamp[:10].replace('-','')
-                        if hostname in allhosts:
-                            if logtime in allhosts[hostname]:
-                                allhosts[hostname][logtime] += [eventid,]
-                            else:
-                                allhosts[hostname][logtime] = [eventid]
+                        eventid = str(eventid) + "sec"
+                    elif eventtype == 'System':
+                        eventid = str(eventid) + "sys"
+                    elif eventtype == 'Application':
+                        eventid = str(eventid) + "app"
+                    hostname = d['Log']['HostName']
+                    timestamp = d['Log']['Event']['Timestamp-Readable-UTC']
+                    logdate = timestamp[:10].replace('-','')
+                    logtime = timestamp[11:19]
+                    #print("logtime:",logtime)
+                    if hostname in allhosts:
+                        if logdate in allhosts[hostname]:
+                            allhosts[hostname][logdate] += [eventid,]
                         else:
-                            allhosts[hostname] = {logtime:[eventid,]}
-                        traincounter += 1
+                            allhosts[hostname][logdate] = [eventid]
+                    else:
+                        allhosts[hostname] = {logdate:[eventid,]}
+                    traincounter += 1
 
                     if traincounter%10000 == 1:
                         print('Current length:', traincounter)
@@ -64,6 +71,6 @@ for host in allhosts:
     for date in allhosts[host]:
         #print(allhosts[host][date])
         seq = host + ':' + ','.join(str(i) for i in allhosts[host][date])
-        fileurl = './output/episode_mining/sec_' + date + '.txt'
+        fileurl = './output/episode_mining_all/sec_' + date + '.txt'
         with open(fileurl,'a') as outfile:
             outfile.write(seq + '\n')
